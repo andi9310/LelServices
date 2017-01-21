8b484b207c191bab594791e3fe058f357d653082
Andrzej Podgorski <andrzej.podgorski@hotmail.com>
2017-01-21 19:04:09 +0100

    LelBuild

---
+++ LelServices/src/LelBuild/Controllers/BuildsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LelBuild.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LelBuild.Controllers
+{
+    [Route("api/[controller]")]
+    public class BuildsController : Controller
+    {
+        // GET api/builds/5/1.0.1
+        [HttpGet("{configurationId}/{buildLabel}")]
+        public Build Get(int configurationId, string buildLabel)
+        {
+            var tests = new List<Test>();
+            var generator = new Random(configurationId);
+            for (var i = 0; i < configurationId * 1000; i++)
+            {
+                tests.Add(new Test() { Command = RandomString(generator) });
+            }
+            return new Build { Configuration = configurationId, Label = buildLabel, Tests = tests };
+        }
+        private static string RandomString(Random generator)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            return new string(Enumerable.Repeat(chars, 3).Select(s => s[generator.Next(s.Length)]).ToArray());
+        }
+    }
+}
---
+++ LelServices/src/LelBuild/Models/Build.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace LelBuild.Models
+{
+    public class Build
+    {
+        public string Label { get; set; }
+        public int Configuration { get; set; }
+        public IEnumerable<Test> Tests { get; set; } 
+    }
+
+    public class Test
+    {
+        public string Command { get; set; }
+    }
+}

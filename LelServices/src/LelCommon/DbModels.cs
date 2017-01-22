8776c03ee9a0744b0e9ff17655654b9f15df3716
Andrzej Podgorski <andrzej.podgorski@hotmail.com>
2017-01-22 15:59:32 +0100

    Sqlagg

---
+++ LelServices/src/LelCommon/DbModels.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace LelCommon
+{
+    public class LelContext : DbContext
+    {
+        public LelContext(DbContextOptions options)
+            : base(options)
+        { }
+
+        public DbSet<Aggregation> Aggregations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=lel_db;User Id=sa;Password=Elo_benc1!; ");
+        }
+    }
+
+    public class Aggregation
+    {
+        [Key]
+        public string Command { get; set; }
+        public int Pass { get; set; }
+        public int Fail { get; set; }
+        public int Error { get; set; }
+
+    }
+}
--- LelServices/src/LelSqlAggregator/Program.cs
+++ LelServices/src/LelSqlAggregator/Program.cs
@@ -1,9 +1,65 @@
-﻿namespace LelSqlAggregator
+﻿using System.Linq;
+using System.Reflection;
+using System.Text;
+using LelCommon;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace LelSqlAggregator
 {
     public class Program
     {
+        private static IModel _channel;
         public static void Main(string[] args)
         {
+            var factory = new ConnectionFactory { HostName = "localhost", Port = 8006, UserName = "guest", Password = "guest" };
+            using (var connection = factory.CreateConnection())
+            using (_channel = connection.CreateModel())
+            {
+                _channel.BasicQos(0, 20, false);
+                _channel.QueueDeclare("lel_stored_sql_agg", true, false, false, null);
+                _channel.ExchangeDeclare("lel_stored", "fanout");
+                _channel.QueueBind("lel_stored_sql_agg", "lel_stored", "");
+
+                var consumer = new EventingBasicConsumer(_channel);
+                consumer.Received += OnMessage;
+                _channel.BasicConsume("lel_stored_sql_agg", false, consumer);
+
+                while (true)
+                {
+                }
+            }
+            // ReSharper disable once FunctionNeverReturns
+        }
+
+        private static void OnMessage(object model, BasicDeliverEventArgs ea)
+        {
+            var result = JsonConvert.DeserializeObject<Result>(Encoding.UTF8.GetString(ea.Body));
+            using (var context = new LelContext(new DbContextOptions<LelContext>()))
+            {
+                context.Database.EnsureCreated();
+
+                var aggregation = context.Aggregations.FirstOrDefault(agg => agg.Command == result.Command);
+                if (aggregation == null)
+                {
+                    aggregation = new Aggregation
+                    {
+                        Command = result.Command,
+                        Pass = 0,
+                        Fail = 0,
+                        Error = 0
+                    };
+                    context.Aggregations.Add(aggregation);
+                }
+                var status = aggregation.GetType().GetProperty(result.Status);
+                var count = (int)status.GetValue(aggregation, null);
+                status.SetValue(aggregation, count + 1);
+                context.SaveChanges();
+            }
+            _channel.BasicAck(ea.DeliveryTag, false);
         }
     }
 }
+
